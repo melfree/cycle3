5ede4edcd2edaafe88fa18308e2a37d67d96eb2f
Melanie Freeman <mdf@andrew.cmu.edu>
2015-10-31 08:53:35 -0400

    Form for changing user attributes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,4 +8,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
     #respond_with resource, location: after_sign_in_path_for(resource)
     redirect_to root_url
   end
+  
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      sign_in resource_name, resource, bypass: true
+    else
+      clean_up_passwords resource
+    end
+    render :edit
+  end
 end

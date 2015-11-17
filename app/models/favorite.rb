94b1fdb3709e0f4f8e356fd45fd94ecf85d3c78a
mogh <mogh@example.com>
2015-11-17 14:22:05 -0500

    fixing syntax error

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,14 +1,14 @@
 class Favorite < ActiveRecord::Base
   #relationships
   belongs_to :user
-end
 
+  #callback
+  before_save :get_favorited
 
-#callback
-before_save :get_favorited
+  def get_favorited
+    self.user_id = current_user.id #id of the person favoriting
+    self.user_name = current_user.name
+    self.user_email = current_user.email
+  end
 
-def get_favorited
-  self.user_id = current_user.id #id of the person favoriting
-  self.user_name = current_user.name
-  self.user_email = current_user.email
 end

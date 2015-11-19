58bc69d8f01110781e94005221775ecc3a878a0a
Melanie Freeman <mdf@andrew.cmu.edu>
2015-11-19 08:28:44 -0500

    Fixed login/registration error bug

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,10 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # This overwrites the Devise function's redirect as a bug fix.
   def create
     build_resource(sign_up_params)
-
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
+    if resource.save
       set_flash_message :notice, :signed_up if is_flashing_format?
       sign_up(resource_name, resource)
        
@@ -16,28 +13,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
     else
       clean_up_passwords resource
       set_minimum_password_length
-      #respond_with resource
+      flash[:alert] = resource.errors.full_messages.first
       render :new
     end
   end
   
   def update
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-
-    resource_updated = update_resource(resource, account_update_params)
-    yield resource if block_given?
-    if resource_updated
-      if is_flashing_format?
-        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
-          :update_needs_confirmation : :updated
-        set_flash_message :notice, flash_key
-      end
+    if update_resource(resource, account_update_params)
+      set_flash_message :notice, :updated
       sign_in resource_name, resource, bypass: true
     else
       clean_up_passwords resource
     end
-    render :edit
+    redirect_to edit_user_registration_url
   end
   
   private
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,8 @@ class Users::SessionsController < Devise::SessionsController
   # This overwrites the Devise function's redirect as a bug fix.
   def create
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    set_flash_message(:notice, :signed_in)
     sign_in(resource_name, resource)
-    yield resource if block_given?
 
     # Action cable requirement
     cookies.signed[:user_id] = resource.id
@@ -16,9 +15,8 @@ class Users::SessionsController < Devise::SessionsController
     # Action cable requirement
     cookies.delete :user_name
     
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
-    yield if block_given?
+    signed_out = sign_out
+    set_flash_message :notice, :signed_out if signed_out
     respond_to_on_destroy
   end
 end

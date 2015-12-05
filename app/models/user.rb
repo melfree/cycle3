acfa7a5548336f841006a30ab47b3c06aff34bd4
Melanie Freeman <mdf@andrew.cmu.edu>
2015-12-05 13:25:54 -0500

    Turning off confirmable user registration for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   
   scope :inactive, ->  { where(status_code: 0)}
   scope :active, ->  { where("status_code <> 0")}

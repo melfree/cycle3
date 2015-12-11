301de83e05f2d4a4bbed690bf1eee620a2b7d304
Melanie Freeman <mdf@andrew.cmu.edu>
2015-12-11 17:06:44 -0500

    Fix users sorting in websockets update?

--- app/jobs/user_relay_job.rb
+++ app/jobs/user_relay_job.rb
@@ -3,9 +3,9 @@ class UserRelayJob < ApplicationJob
     users = User.active
     ActionCable.server.broadcast "users",
       { sellers_html: HomeController.render(partial: 'users/users',
-                                          locals: {data_channel: "sellers", users: users.sellers}),
+                                          locals: {data_channel: "sellers", users: users.sellers.unmatched_first}),
         buyers_html: HomeController.render(partial: 'users/users',
-                                          locals: {data_channel: "buyers", users: users.buyers}),
+                                          locals: {data_channel: "buyers", users: users.buyers.unmatched_first}),
         html_google_map: HomeController.render(partial: 'users/google_map',
                                                locals: { users: users })
       }
